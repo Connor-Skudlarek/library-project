4ba2b4e431da49b8d47827152c67b804a812ce38
Connor Skudlarek <connor.skudlarek@gmail.com>
2022-12-17 10:09:13 -0800

    initial commit
    Also add some initial structure to website.
    Including:
    -HTML Boilerplate
    -New book form
    -Library container
    -Initial javascript
    
    Steps to do:
    -Need to communicate form info to javascript function
    -Need to create constructor function
    -Need to add newly constructed object to library
    -Need to represent objects in library on HTML page

---
+++ index.js
@@ -0,0 +1,18 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function createNewBookForm(){
+    // const submittedForm = document.querySelector("#new-book");
+
+
+}
+
+const newBookButton = document.querySelector(".new-book")
+newBookButton.addEventListener("click", )
