f32e64c3e8014e3297f713a7301748fb501f8816
Connor Skudlarek <connor.skudlarek@gmail.com>
2022-12-17 13:01:22 -0800

    Add book constructor, add book-card to library on creation
    Add ability to create books
    issues:
    
    -Books are added to DOM directly on creation
    -Books have no delete function
    -Books have no 'read' or 'not yet read' toggle
    
    Other additions:
    -Some CSS grid/flex and styling to improve UX
    -Separated the functions in JS to make them more granular

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-let myLibrary = [];
-
-function Book() {
-  // the constructor...
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
 
-function createNewBookForm(){
-    // const submittedForm = document.querySelector("#new-book");
-
-
+function createNewBook() {
+  const submittedName = document.getElementById("new-book-title").value;
+  const submittedAuthor = document.getElementById("new-book-author").value;
+  const newBook = new Book(submittedName, submittedAuthor);
+  return newBook;
 }
 
-const newBookButton = document.querySelector(".new-book")
-newBookButton.addEventListener("click", )
+function createBookCard(book) {
+  const div = document.createElement("div");
+  const paraTitle = document.createElement("p");
+  const paraAuthor = document.createElement("p");
+  div.classList.add("book-card");
+  paraTitle.classList.add("title");
+  paraAuthor.classList.add("author");
+  paraTitle.textContent = "Title: " + book.title;
+  paraAuthor.textContent = "Author: " + book.author;
+  div.append(paraTitle);
+  div.append(paraAuthor);
+  return div;
+}
+
+function addToLibraryInDOM(div) {
+  const library = document.querySelector(".library");
+  library.append(div);
+}
+
+function addNewBook() {
+  const book = createNewBook();
+  addBookToLibrary(book);
+  const div = createBookCard(book);
+  addToLibraryInDOM(div);
+}
+
+let myLibrary = []; // Global variable — Should consider changing this scope.
+
+document.querySelector(".new-book-button").addEventListener("click", addNewBook);
